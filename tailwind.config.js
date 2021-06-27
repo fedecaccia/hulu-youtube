dec1119ec818601b612b0f98d6da05b85515e9f2
Federico Caccia <federicoagustincaccia@gmail.com>
2021-06-27 20:50:51 -0300

    finalproject state

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  mode: "jit",
+  purge: ["./pages/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      screens: {
+        "3xl": "2000px", // extending default breakpoints
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [require("tailwind-scrollbar-hide")],
+}
